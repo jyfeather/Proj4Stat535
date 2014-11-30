4a41397b49d30886753933f162c3e5412143ad4f
Yan <jyfeather@gmail.com>
2014-12-01 07:52:37 +0800

    minor amend

--- R/main.R
+++ R/main.R
@@ -2,7 +2,6 @@
 #                         Parameters Preset  
 #--------------------------------------------------------------------
 rm(list = ls())
-setwd("C:/Users/jyfea_000/Documents/GitHub/Proj4Stat535")
 library(e1071)
 
 #-------------------------------------------------------------------- 
@@ -10,8 +9,10 @@ library(e1071)
 #--------------------------------------------------------------------
 dat.orig <- read.delim(file = "./data/train.txt", header = F)
 x.orig <- dat.orig[,-78]
-y.true <- as.factor(dat.orig[,78])
+y.true <- dat.orig[,78]
 table(y.true) # 99144 0, 856 1, imbalance dataset
+x.orig <- as.matrix(x.orig)
+y.true <- as.matrix(y.true)
 
 #-------------------------------------------------------------------- 
 #                         Classification  
@@ -20,17 +21,27 @@ table(y.true) # 99144 0, 856 1, imbalance dataset
 # other kernels
 # issue: time consuming
 #--------------------------------------------------------------------
-x.train <- x.orig[1:50000,]
 x.test <- x.orig[1:50000,]
-y.train <- y.true[1:50000]
 y.test <- y.true[1:50000]
 
-model.gaussian <- svm(x.train, y.train, kernel = "radial", probability = TRUE) # Gaussian Kernel
-model.linear <- svm(x.train, y.train, kernel = "linear", probability = TRUE)
-model.ploynomial <- svm(x.train, y.train, kernel = "polynomial", probability = TRUE)
-model.sigmoid <- svm(x.train, y.train, kernel = "sigmoid", probability = TRUE)
+# Feature Selection, group lasso
+library(grplasso)
+groupindex <- c(rep(1,7), rep(2,7), rep(3,7), rep(4,7), rep(5,7),
+                rep(6,7), rep(7,7), rep(8,7), rep(9,7), rep(10,7), rep(11,7))
+lambda <- grplasso(x = x.orig, y = y.true, index = groupindex, penscale = sqrt, 
+                   model = LogReg(), center = F, lambda = 10)
 
-pred <- predict(model.linear, x.test, probability = TRUE)
+
+# Model Selection, time consuming
+tuned <- tune.svm(x = x.orig, y = y.true, gamma = 10^(-6:-1), cost = 10^(1:2)) # CV
+
+# Learn Model
+model.gaussian <- svm(x.orig, y.true, kernel = "radial", probability = TRUE) # Gaussian Kernel
+#model.linear <- svm(x.train, y.train, kernel = "linear", probability = TRUE)
+#model.ploynomial <- svm(x.train, y.train, kernel = "polynomial", probability = TRUE)
+#model.sigmoid <- svm(x.train, y.train, kernel = "sigmoid", probability = TRUE)
+
+pred <- predict(model.gaussian, x.test, probability = TRUE)
 table(pred, y.test)
 
 #-------------------------------------------------------------------- 
