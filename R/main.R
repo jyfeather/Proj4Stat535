60e1bfafca361cc82814d00f28e53374be9a9c9a
Yan <jyfeather@gmail.com>
2014-12-01 18:28:39 -0800

    minor

--- R/main.R
+++ R/main.R
@@ -5,6 +5,51 @@ rm(list = ls())
 library(e1071)
 
 #-------------------------------------------------------------------- 
+#                         Defined Functions  
+# 1. FeatureExpansion(dat): generate new features
+# 2. TrainReduction(train, n1, n2): reduce training set sample size
+#--------------------------------------------------------------------
+FeatureExpansion <- function(dat) {
+  # origin: 11 features * 7 periods
+  # plus min, mean, median, max, daily increasment
+  n <- nrow(dat); p <- ncol(dat); t <- 7
+  for (i in 1:(p/7)) {
+    tmp <- dat[,((i-1)*7+1):(7*i)]
+    #dat <- cbind(dat, apply(tmp, 1, function(x)return(min(x))))
+    #dat <- cbind(dat, apply(tmp, 1, function(x)return(max(x))))
+    dat <- cbind(dat, apply(tmp, 1, function(x)return(mean(x))))
+    #dat <- cbind(dat, apply(tmp, 1, function(x)return(median(x))))
+  }  
+  # appended after 77 + 4*11 = 121
+  for (i in 1:(p/7)) {
+    tmp <- dat[,((i-1)*7+1):(7*i)]
+    for (j in 2:7) {
+      dat <- cbind(dat, tmp[,j] - tmp[,j-1])
+    }
+  }
+  return(dat)
+}
+
+TrainReduction <- function(train, y) {
+  pos <- which(y == 1)  # keep the imbalanced dataset
+  train.new <- train[-pos,]; y.new <- y[-pos]
+  train.new.clusters <- kmeans(train.new, centers = 10)
+  clu.del <- c()
+  for (i in 1:10) { # delete clusters < 500
+    size <- train.new.clusters$size[i]
+    if (size <= 500) {
+      clu.del <- c(clu.del, which(train.new.clusters$cluster == i))
+    } else {
+      clus <- which(train.new.clusters$cluster == i)
+      clu.del <- c(clu.del, sample(clus, size * 0.7))
+    }
+  }
+  train.new <- train.new[-clu.del,]; y.new <- y.new[-clu.del]
+  train.new <- rbind(train.new, train[pos,]); y.new <- c(y.new, y[pos])
+  dat <- cbind(train.new, y.new)
+  return(dat)
+}
+#-------------------------------------------------------------------- 
 #                         Data Input  
 #--------------------------------------------------------------------
 dat.orig <- read.delim(file = "./data/train.txt", header = F)
@@ -21,33 +66,35 @@ y.true <- as.matrix(y.true)
 # other kernels
 # issue: time consuming
 #--------------------------------------------------------------------
-x.test <- x.orig[1:50000,]
+# Feature Generation, 187 features in total
+x.train <- FeatureExpansion(x.orig)
+
+x.test <- x.train[1:50000,]
 y.test <- y.true[1:50000]
 
-# Feature Selection, group lasso
-library(grplasso)
-groupindex <- c(rep(1,7), rep(2,7), rep(3,7), rep(4,7), rep(5,7),
-                rep(6,7), rep(7,7), rep(8,7), rep(9,7), rep(10,7), rep(11,7))
-lambda <- grplasso(x = x.orig, y = y.true, index = groupindex, penscale = sqrt, 
-                   model = LogReg(), center = F, lambda = 10)
-
+# Train dataset dimension reduction
+dat.red <- TrainReduction(x.train, y.true)
+x.train <- dat.red[,1:154]; y.true <- dat.red[,155]
 
 # Model Selection, time consuming
-tuned <- tune.svm(x = x.orig, y = y.true, gamma = 10^(-6:-1), cost = 10^(1:2)) # CV
+tuned <- tune.svm(x = x.train[1:10000,], y = y.true[1:10000,], 
+                  gamma = 10^(-2:-1), cost = 10^(1:2),
+                  tunecontrol = tune.control(sampling = "cross", cross = 5)) # CV
 
 # Learn Model
-model.gaussian <- svm(x.orig, y.true, kernel = "radial", probability = TRUE) # Gaussian Kernel
+model.gaussian <- svm(x.train, y.true, kernel = "radial", probability = TRUE) # Gaussian Kernel
 #model.linear <- svm(x.train, y.train, kernel = "linear", probability = TRUE)
 #model.ploynomial <- svm(x.train, y.train, kernel = "polynomial", probability = TRUE)
 #model.sigmoid <- svm(x.train, y.train, kernel = "sigmoid", probability = TRUE)
 
-pred <- predict(model.gaussian, x.test, probability = TRUE)
-table(pred, y.test)
+pred.gaussian <- predict(model.gaussian, x.test, decision.values = TRUE, probability = TRUE)
+table(pred.gaussian, y.test)
 
 #-------------------------------------------------------------------- 
 #                         Performance Estimation  
 # imbalance dataset, use ROC/AUC as performance evaluator
 #--------------------------------------------------------------------
+pred <- pred.gaussian
 library(ROCR)
 y.prob <- attr(pred, "probabilities")[,2]
 roc.pred <- prediction(y.prob, y.test)
